86c327c4c0ff9de8bf4f11aedb5ae4bf6706af50
Christopher Patti <feoh@feoh.org>
2026-04-26 19:57:29 -0400

    feat: Skeleton of a CLI Bluesky client

--- main.py
+++ main.py
@@ -1,6 +1,67 @@
-def main():
-    print("Hello from clay-pigeon!")
+import typer
+import atproto
+from rich.prompt import Prompt
+from rich import print
+from pathlib import Path
+import tomllib
+import tomli_w
 
 
+
+config_dir = Path.home() / ".config" / "clay-pigeon"
+config_dir.mkdir(parents=True, exist_ok=True)
+config_file_path = config_dir / "config.toml"
+config_file = str(config_file_path)
+
+app = typer.Typer(no_args_is_help=True)
+
+
+def get_client():
+    client: atproto.Client = atproto.Client()
+    return client
+
+def get_config() -> dict[str,str]:
+    with open(config_file,"rb") as cf:
+        config_dict = tomllib.load(cf)
+        return config_dict
+
+
+def get_profile():
+    client = get_client()
+    config_dict = get_config()
+    bluesky_username = config_dict['username']
+    bluesky_password = config_dict['password']
+    profile = client.login(bluesky_username, bluesky_password)
+    return profile
+    
+
+
+"""
+configure: Write clay-pigeon configuration file
+
+Note: Since users are explicitly asking for this, we overwrite existing configurations.
+"""
+@app.command()
+def configure():
+    bluesky_username = Prompt.ask("[blue]Bluesky Username[/blue]")
+    bluesky_password = Prompt.ask("[red]Bluesky Password[/red]", password=True)
+
+    claypigeon_config = { "username": bluesky_username, "password": bluesky_password }
+
+    with open(config_file, "wb") as f:
+        tomli_w.dump(claypigeon_config, f)
+
+
+"""
+timeline: Diaplay user's timeline
+"""
+@app.command()
+def timeline():
+    client = get_client()
+    profile = get_profile()
+    # Temporary! Actual logic to print posts coming soon!
+    print(profile)
+
+    
 if __name__ == "__main__":
-    main()
+    app()
